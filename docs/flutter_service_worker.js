00a3470851619bae548e088576931a4017ec1b5e
Kaue Murakami <kauetmurakami@gmail.com>
2021-08-14 18:35:07 -0300

    add url strategy

--- docs/flutter_service_worker.js
+++ docs/flutter_service_worker.js
@@ -3,16 +3,16 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
-  "main.dart.js": "dfb4320cbbe2cc53ea3ed4d8c9e17e6f",
+  "main.dart.js": "b79afdcfd20c7aed130c044a221e3387",
 "manifest.json": "ad1e11254912c08da7ce75aa2bbe2b8f",
-"index.html": "8079fda1752fc80bfe3b8265ddffad5e",
-"/": "8079fda1752fc80bfe3b8265ddffad5e",
+"index.html": "2071f052de3af96550510fcb54d29475",
+"/": "2071f052de3af96550510fcb54d29475",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "assets/FontManifest.json": "dc3d03800ccca4601324923c0b1d6d57",
 "assets/fonts/MaterialIcons-Regular.otf": "4e6447691c9509f7acdbf8a931a85ca1",
 "assets/AssetManifest.json": "2efbb41d7877d10aac9d091f58ccd7b9",
-"assets/NOTICES": "b169f1cc02395bee7a6b38d928b2657b",
+"assets/NOTICES": "577901dd59b95966f21e6b788c68c04b",
 "assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "6d342eb68f170c97609e9da345464e5e",
 "version.json": "c6f7282272d2dec065ce7f95b083c2cf",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796"
